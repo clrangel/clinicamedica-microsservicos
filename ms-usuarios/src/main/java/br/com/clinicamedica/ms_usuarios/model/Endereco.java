0eb713e4fc6d49ae701b711a6c55eaa463a9c267
Camilo LR <autumndrums@hotmail.com>
2025-10-20 16:19:43 -0300

    MS-USUARIOS: Entidade Endereço

---
+++ ms-usuarios/src/main/java/br/com/clinicamedica/ms_usuarios/model/Endereco.java
@@ -0,0 +1,22 @@
+package br.com.clinicamedica.ms_usuarios.model;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable //Indica que a classe não terá tabela própria, mas será incorporada em outra entidade (Médico e Paciente).
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String cidade;
+    private String uf;
+}
