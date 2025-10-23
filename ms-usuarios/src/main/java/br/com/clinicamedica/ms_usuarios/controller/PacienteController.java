121ca91507aedb12021bac52e640f8102b76ce96
Camilo LR <autumndrums@hotmail.com>
2025-10-23 14:05:50 -0300

    MS-USUARIOS: Paciente - PacienteService e PacienteController - Método Cadastrar Paciente

---
+++ ms-usuarios/src/main/java/br/com/clinicamedica/ms_usuarios/controller/PacienteController.java
@@ -0,0 +1,31 @@
+package br.com.clinicamedica.ms_usuarios.controller;
+
+import br.com.clinicamedica.ms_usuarios.dto.PacienteRequestDTO;
+import br.com.clinicamedica.ms_usuarios.dto.PacienteResponseDTO;
+import br.com.clinicamedica.ms_usuarios.service.PacienteService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/pacientes")
+public class PacienteController {
+
+    @Autowired
+    private PacienteService service;
+
+    @PostMapping
+    public ResponseEntity<PacienteResponseDTO> cadastrarPaciente(@Valid @RequestBody PacienteRequestDTO dto){
+        PacienteResponseDTO pacienteCriado = service.cadastrarPaciente(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(pacienteCriado);
+    }
+
+}
+
---
+++ ms-usuarios/src/main/java/br/com/clinicamedica/ms_usuarios/service/PacienteService.java
@@ -0,0 +1,29 @@
+package br.com.clinicamedica.ms_usuarios.service;
+
+import br.com.clinicamedica.ms_usuarios.dto.PacienteRequestDTO;
+import br.com.clinicamedica.ms_usuarios.dto.PacienteResponseDTO;
+import br.com.clinicamedica.ms_usuarios.mapper.PacienteMapper;
+import br.com.clinicamedica.ms_usuarios.model.Paciente;
+import br.com.clinicamedica.ms_usuarios.repository.PacienteRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class PacienteService {
+
+    @Autowired
+    private PacienteRepository repository;
+
+    @Autowired
+    private PacienteMapper mapper;
+
+    public PacienteResponseDTO cadastrarPaciente(PacienteRequestDTO dto){
+
+        Paciente paciente = mapper.toEntity(dto);
+        repository.save(paciente);
+        return mapper.toDto(paciente);
+    }
+    
+}
