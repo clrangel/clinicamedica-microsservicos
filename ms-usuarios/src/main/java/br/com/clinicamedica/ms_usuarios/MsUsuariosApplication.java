4110cafe33a0ca4718844305b1daa9dfd67e35a1
Camilo LR <autumndrums@hotmail.com>
2025-10-28 15:30:11 -0300

    MS-USUARIOS: Configuração do Eureka Client

--- ms-usuarios/src/main/java/br/com/clinicamedica/ms_usuarios/MsUsuariosApplication.java
+++ ms-usuarios/src/main/java/br/com/clinicamedica/ms_usuarios/MsUsuariosApplication.java
@@ -2,8 +2,10 @@ package br.com.clinicamedica.ms_usuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsUsuariosApplication {
 
 	public static void main(String[] args) {
