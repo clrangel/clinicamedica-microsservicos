b8203fa16187cb0d58f7f57ec3782047c0b3a170
Camilo LR <autumndrums@hotmail.com>
2025-10-28 14:55:39 -0300

    Configurações iniciais do Service Registry: Eureka Server

--- service-registry-clinicamedica/src/main/java/br/com/clinicamedica/service_registry_clinicamedica/ServiceRegistryClinicamedicaApplication.java
+++ service-registry-clinicamedica/src/main/java/br/com/clinicamedica/service_registry_clinicamedica/ServiceRegistryClinicamedicaApplication.java
@@ -2,8 +2,10 @@ package br.com.clinicamedica.service_registry_clinicamedica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceRegistryClinicamedicaApplication {
 
 	public static void main(String[] args) {
