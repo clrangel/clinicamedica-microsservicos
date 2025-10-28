c732952d02f9005da099149bcb94064456ed9ad7
Camilo LR <autumndrums@hotmail.com>
2025-10-28 14:40:31 -0300

    Criando o Service Registry Clinica Medica

---
+++ service-registry-clinicamedica/src/main/java/br/com/clinicamedica/service_registry_clinicamedica/ServiceRegistryClinicamedicaApplication.java
@@ -0,0 +1,13 @@
+package br.com.clinicamedica.service_registry_clinicamedica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegistryClinicamedicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryClinicamedicaApplication.class, args);
+	}
+
+}
---
+++ service-registry-clinicamedica/src/test/java/br/com/clinicamedica/service_registry_clinicamedica/ServiceRegistryClinicamedicaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.clinicamedica.service_registry_clinicamedica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryClinicamedicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
